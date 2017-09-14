056f0968573a70b573c9ac36d8f79bb99c8a28dc
BReily89 <BrandonReily.br@gmail.com>
2017-09-14 16:00:53 -0400

    potent potables questions and functions

--- jeopardy.js
+++ jeopardy.js
@@ -3,7 +3,7 @@ const earthQuestions = [
     {
         question: 'Earth is also know as *blank rock* from the sun',
         answer: '4th',
-        choices:'1st, 3ed, 4th, 5th',
+        choices: '1st, 3ed, 4th, 5th',
         points: 100,
     },
     {
@@ -25,38 +25,38 @@ const earthQuestions = [
         points: 400,
     },
 ];
-   
- const windQuestions = [
-        {
-            question: 'Wind is caused by',
-            answer: 'Atmospheric Pressure',
-            choices:'Atmospheric pressure, God blowing dandelions, butterfly wings, Hurricanes',
-            points: 100,
-        },
-        {
-            question: 'This mountain recorded the highest wind speed  ',
-            answer: 'Mount Washington',
-            choices: 'Mount Everest , Mount Kilamanjaro, Mount Trashmoore, Mount Washington',
-            points: 200,
-        },
-        {
-            question: 'This is city has the moniker of the Windy city',
-            answer: 'Chicago',
-            choices: 'Boston, Windyton, Chicago, New York',
-            points: 300,
-        },
-        {
-            question: 'All we are is dust in the wind is a line from which stately band',
-            answer: 'Kansas',
-            choices: 'Georgia, Florida, Kansas, California',
-            points: 400,
-        },
+
+const windQuestions = [
+    {
+        question: 'Wind is caused by',
+        answer: 'Atmospheric Pressure',
+        choices: 'Atmospheric pressure, God blowing dandelions, butterfly wings, Hurricanes',
+        points: 100,
+    },
+    {
+        question: 'This mountain recorded the highest wind speed  ',
+        answer: 'Mount Washington',
+        choices: 'Mount Everest , Mount Kilamanjaro, Mount Trashmoore, Mount Washington',
+        points: 200,
+    },
+    {
+        question: 'This is city has the moniker of the Windy city',
+        answer: 'Chicago',
+        choices: 'Boston, Windyton, Chicago, New York',
+        points: 300,
+    },
+    {
+        question: 'All we are is dust in the wind is a line from which stately band',
+        answer: 'Kansas',
+        choices: 'Georgia, Florida, Kansas, California',
+        points: 400,
+    },
 ];
 const fireQuestions = [
     {
         question: 'This Ray Bradbury book is titled for the tempeture at which fire burns paper',
         answer: 'Farenheint 451',
-        choices:'100K, fahrenheit 451, 212C, 98 degrees',
+        choices: '100K, fahrenheit 451, 212C, 98 degrees',
         points: 100,
     },
     {
@@ -78,8 +78,36 @@ const fireQuestions = [
         points: 400,
     },
 ];
+const potentPotablesQuestions = [
+    {
+        question: 'Usually made from a base of apricot pits, which sweet, almond-flavored Italian liqueur is popularized by the Disaronno brand?',
+        answer: 'Amaretto',
+        choices: 'Sambuca, Amaretto, Absinth, Ramazzotti',
+        points: 100,
+    },
+    {
+        question: ' Sharing its name with a NYC borough, which cocktail is made with whiskey, sweet vermouth, bitters, and typically garnished with a Maraschino cherry? ',
+        answer: 'Manhattan',
+        choices: 'Manhattan, Brooklyn, Yonkers, Harlem',
+        points: 200,
+    },
+    {
+        question: 'What is the base spirit in an "Industry Sour"',
+        answer: 'Fernet',
+        choices: 'Fernet, Chartreuce, Vodka, Whiskey',
+        points: 300,
+    },
+    {
+        question: 'In the 2003 movie "Lost in Translation", which whiskey brand is Bob Harris (portrayed by Bill Murray) hired to advertise in Tokyo for $2 million?',
+        answer: 'Jameson, Nikka, Suntory, Courvoisi',
 
- $('.earthOneHundred').click( function($event) {
+        choices: 'Novemeber, September, December, August',
+        points: 400,
+    },
+];
+
+
+$('.earthOneHundred').click(function ($event) {
     $('.earthOneHundred').addClass('disabled');
     const y = prompt(earthQuestions[0].question, `Type ${earthQuestions[0].choices}`);
     if (y === earthQuestions[0].answer) {
@@ -94,7 +122,7 @@ const fireQuestions = [
     }
 });
 
-$('.earthTwoHundred').click( function($event) {
+$('.earthTwoHundred').click(function ($event) {
     $('.earthTwoHundred').addClass('disabled');
     const y = prompt(earthQuestions[1].question, `Type ${earthQuestions[1].choices}`);
     if (y === earthQuestions[1].answer) {
@@ -108,7 +136,7 @@ $('.earthTwoHundred').click( function($event) {
         $('.scoreUpdate').html(score);
     }
 });
-$('.earthThreeHundred').click( function($event) {
+$('.earthThreeHundred').click(function ($event) {
     $('.earthThreeHundred').addClass('disabled');
     const y = prompt(earthQuestions[2].question, `Type ${earthQuestions[2].choices}`);
     if (y === earthQuestions[2].answer) {
@@ -122,7 +150,7 @@ $('.earthThreeHundred').click( function($event) {
         $('.scoreUpdate').html(score);
     }
 });
-$('.earthFourHundred').click( function($event) {
+$('.earthFourHundred').click(function ($event) {
     $('.earthFourHundred').addClass('disabled');
     const y = prompt(earthQuestions[3].question, `Type ${earthQuestions[3].choices}`);
     if (y === earthQuestions[3].answer) {
@@ -137,7 +165,7 @@ $('.earthFourHundred').click( function($event) {
     }
 });
 // Functions about Wind
-$('.windOneHundred').click( function($event) {
+$('.windOneHundred').click(function ($event) {
     $('.windOneHundred').addClass('disabled');
     const y = prompt(windQuestions[0].question, `Type ${windQuestions[0].choices}`);
     if (y === windQuestions[0].answer) {
@@ -151,7 +179,7 @@ $('.windOneHundred').click( function($event) {
         $('.scoreUpdate').html(score);
     }
 });
-$('.windTwoHundred').click( function($event) {
+$('.windTwoHundred').click(function ($event) {
     $('.windTwoHundred').addClass('disabled');
     const y = prompt(windQuestions[1].question, `Type ${windQuestions[1].choices}`);
     if (y === windQuestions[1].answer) {
@@ -165,7 +193,7 @@ $('.windTwoHundred').click( function($event) {
         $('.scoreUpdate').html(score);
     }
 });
-$('.windThreeHundred').click( function($event) {
+$('.windThreeHundred').click(function ($event) {
     $('.windThreeHundred').addClass('disabled');
     const y = prompt(windQuestions[2].question, `Type ${windQuestions[2].choices}`);
     if (y === windQuestions[2].answer) {
@@ -179,7 +207,7 @@ $('.windThreeHundred').click( function($event) {
         $('.scoreUpdate').html(score);
     }
 });
-$('.windFourHundred').click( function($event) {
+$('.windFourHundred').click(function ($event) {
     $('.windFourHundred').addClass('disabled');
     const y = prompt(windQuestions[3].question, `Type ${windQuestions[3].choices}`);
     if (y === windQuestions[3].answer) {
@@ -193,38 +221,78 @@ $('.windFourHundred').click( function($event) {
         $('.scoreUpdate').html(score);
     }
 });
-        $('.fireOneHundred').click( function($event) {
-            $('.fireOneHundred').addClass('disabled');
-            const y = prompt(fireQuestions[0].question, `Type ${fireQuestions[0].choices}`);
-            if (y === fireQuestions[0].answer) {
-                alert('Correct!');
-                score += Questions[0].points;
-                $('.scoreUpdate').html(score);
-            }
-            else {
-                alert('Incorrect');
-                score -= Questions[0].points;
-                $('.scoreUpdate').html(score);
-            }
-        });
-        
-        $('.fireTwoHundred').click( function($event) {
-            $('.fireTwoHundred').addClass('disabled');
-            const y = prompt(fireQuestions[1].question, `Type ${fireQuestions[1].choices}`);
-            if (y === fireQuestions[1].answer) {
-                alert('Correct!');
-                score += Questions[1].points;
-                $('.scoreUpdate').html(score);
-            }
-            else {
-                alert('Incorrect');
-                score -= Questions[1].points;
-                $('.scoreUpdate').html(score);
-            }
-        });
-        $('.fireThreeHundred').click( function($event) {
-            $('.fireThreeHundred').addClass('disabled');
-            const y = prompt(fireQuestions[2].question, `Type ${fireQuestions[2].choices}`);
+$('.fireOneHundred').click(function ($event) {
+    $('.fireOneHundred').addClass('disabled');
+    const y = prompt(fireQuestions[0].question, `Type ${fireQuestions[0].choices}`);
+    if (y === fireQuestions[0].answer) {
+        alert('Correct!');
+        score += Questions[0].points;
+        $('.scoreUpdate').html(score);
+    }
+    else {
+        alert('Incorrect');
+        score -= Questions[0].points;
+        $('.scoreUpdate').html(score);
+    }
+});
+
+$('.fireTwoHundred').click(function ($event) {
+    $('.fireTwoHundred').addClass('disabled');
+    const y = prompt(fireQuestions[1].question, `Type ${fireQuestions[1].choices}`);
+    if (y === fireQuestions[1].answer) {
+        alert('Correct!');
+        score += Questions[1].points;
+        $('.scoreUpdate').html(score);
+    }
+    else {
+        alert('Incorrect');
+        score -= Questions[1].points;
+        $('.scoreUpdate').html(score);
+    }
+});
+$('.fireThreeHundred').click(function ($event) {
+    $('.fireThreeHundred').addClass('disabled');
+    const y = prompt(fireQuestions[2].question, `Type ${fireQuestions[2].choices}`);
+    if (y === earthQuestions[2].answer) {
+        alert('Correct!');
+        score += Questions[2].points;
+        $('.scoreUpdate').html(score);
+    }
+    else {
+        alert('Incorrect');
+        score -= Questions[2].points;
+        $('.scoreUpdate').html(score);
+    }
+});
+$('.fireFourHundred').click(function ($event) {
+    $('.fireFourHundred').addClass('disabled');
+    const y = prompt(fireQuestions[3].question, `Type ${fireQuestions[3].choices}`);
+    if (y === fireQuestions[3].answer) {
+        alert('Correct!');
+        score += Questions[3].points;
+        $('.scoreUpdate').html(score);
+    }
+    else {
+        alert('Incorrect');
+        score -= Questions[3].points;
+        $('.scoreUpdate').html(score);
+    }
+});
+$('.potentPotablesOneHundred').click(function ($event) {
+    $('.potentPotablesOneHundred').addClass('disabled');
+    const y = prompt(potentPotablesQuestions[2].question, `Type ${potentPotablesQuestions[2].choices}`);
+    if (y === potentPotablesQuestions[2].answer) {
+        alert('Correct!');
+        score += Questions[2].points;
+        $('.scoreUpdate').html(score);
+    }
+    else {
+        alert('Incorrect');
+        score -= Questions[2].points;
+        $('.scoreUpdate').html(score);
+        $('.potentPotablesTwoHundred').click(function ($event) {
+            $('.potentPotablesTwoHundred').addClass('disabled');
+            const y = prompt(potentPotablesQuestions[2].question, `Type ${potentPotablesQuestions[2].choices}`);
             if (y === earthQuestions[2].answer) {
                 alert('Correct!');
                 score += Questions[2].points;
@@ -236,10 +304,26 @@ $('.windFourHundred').click( function($event) {
                 $('.scoreUpdate').html(score);
             }
         });
-        $('.fireFourHundred').click( function($event) {
-            $('.fireFourHundred').addClass('disabled');
-            const y = prompt(fireQuestions[3].question, `Type ${fireQuestions[3].choices}`);
-            if (y === fireQuestions[3].answer) {
+
+
+        $('.potentPotablesThreeHundred').click(function ($event) {
+            $('.potentPotablesThreeHundred').addClass('disabled');
+            const y = prompt(potentPotablesQuestions[2].question, `Type ${potentPotablesQuestions[2].choices}`);
+            if (y === earthQuestions[2].answer) {
+                alert('Correct!');
+                score += Questions[2].points;
+                $('.scoreUpdate').html(score);
+            }
+            else {
+                alert('Incorrect');
+                score -= Questions[2].points;
+                $('.scoreUpdate').html(score);
+            }
+        });
+        $('.potentPotablesFourHundred').click(function ($event) {
+            $('.potentPotablesFourHundred').addClass('disabled');
+            const y = prompt(potentPotablesQuestions[3].question, `Type ${potentPotablesQuestions[3].choices}`);
+            if (y === potentPotablesQuestions[3].answer) {
                 alert('Correct!');
                 score += Questions[3].points;
                 $('.scoreUpdate').html(score);
@@ -250,3 +334,5 @@ $('.windFourHundred').click( function($event) {
                 $('.scoreUpdate').html(score);
             }
         });
+    }
+});
